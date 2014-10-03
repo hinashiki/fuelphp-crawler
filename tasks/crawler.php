df50e115afb7d0ac837aaec7e4b1aef86005860f
Oisyna Application <info@mail.oisyna.com>
2014-10-03 07:06:57 +0000

    パスの修正

--- tasks/crawler.php
+++ tasks/crawler.php
@@ -6,7 +6,7 @@ class Crawler
 	public function run()
 	{
 		\Cli::write(\Cli::color('this is a test. get yahoo.co.jp', 'light_blue'));
-		$html = Crawler\Crawl::curl('http://www.yahoo.co.jp');
+		$html = \Crawler\Crawl::curl('http://www.yahoo.co.jp');
 		\Cli::write($html->plaintext);
 	}
 }
