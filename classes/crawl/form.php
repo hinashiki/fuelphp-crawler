aeff44c8b4df350792ae39244bc086ae3504491d
hinashiki <aono@slash-one.com>
2015-02-05 15:30:55 +0900

    Form post送信のクロールでプロキシ利用を選択できるように修正

--- classes/crawl/form.php
+++ classes/crawl/form.php
@@ -8,7 +8,7 @@ class Crawl_Form extends \Crawl
 	 * @param string $uri
 	 * @param array $data formData
 	 */
-	public static function post($uri, $data)
+	public static function post($uri, $data, $use_proxy = false)
 	{
 		static::$_request_headers = array(
 			'Content-Type' => 'application/x-www-form-urlencoded',
@@ -16,6 +16,6 @@ class Crawl_Form extends \Crawl
 		);
 		static::$_request_method = 'post';
 		static::$_request_body = (array) $data;
-		return self::curl($uri);
+		return self::curl($uri, $use_proxy);
 	}
 }
